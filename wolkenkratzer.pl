d9de6bd33e3d99ff9399c4ab5bdee7f8e01d8b5c
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2019-11-21 09:28:26 -0300

    Acknowledge parks

--- wolkenkratzer.pl
+++ wolkenkratzer.pl
@@ -1,28 +1,32 @@
 :- use_module(library(clpfd)).
 
-wolkenkratzer(Board, (Upper, Left, Lower, Right), N) :-
+wolkenkratzer(Board, (Upper, Left, Lower, Right), Max) :-
+    length(Board, N),
+    Min is 1 - (N - Max),
     flatten(Board, Cells),
-    Cells ins 1..N,
+    Cells ins Min..Max,
     Rows = Board,
     transpose(Rows, Columns),
+    maplist(all_distinct, Rows),
+    maplist(all_distinct, Columns),
     maplist(reverse, Rows, ReversedRows),
     maplist(reverse, Columns, ReversedColumns),
     maplist(skyscrapers, Rows, Left),
     maplist(skyscrapers, ReversedRows, Right),
-    maplist(skyscrapers, Columns, Upper),
     maplist(skyscrapers, ReversedColumns, Lower),
-    maplist(all_distinct, Rows),
-    maplist(all_distinct, Columns),
+    maplist(skyscrapers, Columns, Upper),
     maplist(label, Rows).
 
 skyscrapers(_, 0) :- !.
+skyscrapers([0|Rest], Constraint) :- skyscrapers(Rest, 0, Constraint, 0), !.
 skyscrapers([First|Rest], Constraint) :-
+    First #> 0,
     skyscrapers(Rest, First, Constraint, 1).
 
 skyscrapers([], _, Constraint, Constraint).
 skyscrapers([First|Rest], Max, Constraint, Num) :-
     (First #> Max,
-     Seen #= Num + 1,
+     Seen is Num + 1,
      skyscrapers(Rest, First, Constraint, Seen),
      !);
     (First #=< Max,
