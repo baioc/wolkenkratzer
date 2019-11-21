2a61dade0271a727ece490a46f05aeee8c6d7f92
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2019-11-21 11:07:52 -0300

    Instance test queries

--- wolkenkratzer.pl
+++ wolkenkratzer.pl
@@ -13,26 +13,70 @@ wolkenkratzer(Board, (Upper, Left, Lower, Right), Max) :-
     maplist(reverse, Columns, ReversedColumns),
     maplist(skyscrapers, Rows, Left),
     maplist(skyscrapers, ReversedRows, Right),
-    maplist(skyscrapers, ReversedColumns, Lower),
     maplist(skyscrapers, Columns, Upper),
-    maplist(label, Rows).
+    maplist(skyscrapers, ReversedColumns, Lower),
+    maplist(label, Board).
 
-diagonal(Rows, Diag) :- length(Rows,N), range(1,N,Indices), maplist(nth1, Indices, Rows, Diag).
-
-range(Low, High, [Low]) :- High =< Low, !.
-range(Low, High, [Low|Range]) :- High > Low, Next is Low + 1, range(Next, High, Range), !.
-
-skyscrapers(_, 0) :- !.
+skyscrapers(_, 0).
 skyscrapers([0|Rest], Constraint) :- skyscrapers(Rest, 0, Constraint, 0).
 skyscrapers([First|Rest], Constraint) :-
-    First #> 0,
-    skyscrapers(Rest, First, Constraint, 1).
+    First #> 0, skyscrapers(Rest, First, Constraint, 1).
 
 skyscrapers([], _, Constraint, Constraint).
-skyscrapers([First|Rest], Max, Constraint, Num) :-
-    (First #> Max,
-     Seen is Num + 1,
-     skyscrapers(Rest, First, Constraint, Seen),
-     !);
-    (First #=< Max,
-     skyscrapers(Rest, Max, Constraint, Num)).
+skyscrapers([First|Rest], Max, Constraint, Seen) :-
+    (First #> Max, Num #= Seen + 1, skyscrapers(Rest,First,Constraint,Num), !);
+    (First #=< Max, skyscrapers(Rest,Max,Constraint,Seen), !).
+
+diagonal(Rows, Diag) :-
+    length(Rows, N), range(0, N, Indices), maplist(nth0, Indices, Rows, Diag).
+
+range(Low, High, []) :- High #=< Low.
+range(Low, High, [Low|Range]) :-
+    High #> Low, Next #= Low + 1, range(Next, High, Range).
+
+
+%% https://www.janko.at/Raetsel/Wolkenkratzer/242.a.htm
+% ?- Board =  [[_,_,_,_],
+%              [_,_,_,_],
+%              [_,_,_,_],
+%              [_,_,_,_]],
+%    Board = [A,B,C,D],
+%    wolkenkratzer(Board, ([0,0,4,0], [0,0,0,0], [0,2,0,0], [1,4,0,0]), 4).
+%
+% Board = [[3, 2, 1, 4], [4, 3, 2, 1], [1, 4, 3, 2], [2, 1, 4, 3]],
+% A = [3, 2, 1, 4],
+% B = [4, 3, 2, 1],
+% C = [1, 4, 3, 2],
+% D = [2, 1, 4, 3] ;
+% false.
+
+%% https://www.janko.at/Raetsel/Wolkenkratzer/383.a.htm
+% ?- Board =  [[_,_,_,_],
+%              [_,_,_,_],
+%              [_,_,_,_],
+%              [_,_,_,_]],
+%    Board = [A,B,C,D],
+%    wolkenkratzer(Board, ([0,0,0,1], [0,2,0,2], [0,0,3,0], [0,0,0,0]), 3).
+%
+% Board = [[1, 2, 0, 3], [0, 1, 3, 2], [3, 0, 2, 1], [2, 3, 1, 0]],
+% A = [1, 2, 0, 3],
+% B = [0, 1, 3, 2],
+% C = [3, 0, 2, 1],
+% D = [2, 3, 1, 0] ;
+% false.
+
+%% https://www.janko.at/Raetsel/Wolkenkratzer/005.a.htm
+% ?- Board =  [[_,_,_,_],
+%              [_,_,_,_],
+%              [_,_,_,_],
+%              [_,_,_,_]],
+%    Board = [A,B,C,D],
+%    wolkenkratzer(Board, ([0,0,0,0], [1,2,3,0], [0,0,0,0], [0,0,0,0]), 4).
+% %
+% Board = [[4, 1, 2, 3], [3, 4, 1, 2], [2, 3, 4, 1], [1, 2, 3, 4]],
+% A = [4, 1, 2, 3],
+% B = [3, 4, 1, 2],
+% C = [2, 3, 4, 1],
+% D = [1, 2, 3, 4] ;
+% false.
+%% @FIXME: non-distinct diagonals
