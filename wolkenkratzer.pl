131f612736cf08999ee273deca478cc6462c78a5
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2019-11-21 10:01:06 -0300

    Acquire diagonals

--- wolkenkratzer.pl
+++ wolkenkratzer.pl
@@ -17,8 +17,13 @@ wolkenkratzer(Board, (Upper, Left, Lower, Right), Max) :-
     maplist(skyscrapers, Columns, Upper),
     maplist(label, Rows).
 
+diagonal(Rows, Diag) :- length(Rows,N), range(1,N,Indices), maplist(nth1, Indices, Rows, Diag).
+
+range(Low, High, [Low]) :- High =< Low, !.
+range(Low, High, [Low|Range]) :- High > Low, Next is Low + 1, range(Next, High, Range), !.
+
 skyscrapers(_, 0) :- !.
-skyscrapers([0|Rest], Constraint) :- skyscrapers(Rest, 0, Constraint, 0), !.
+skyscrapers([0|Rest], Constraint) :- skyscrapers(Rest, 0, Constraint, 0).
 skyscrapers([First|Rest], Constraint) :-
     First #> 0,
     skyscrapers(Rest, First, Constraint, 1).
